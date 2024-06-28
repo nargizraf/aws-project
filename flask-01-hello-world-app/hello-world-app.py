0dfe3aa803dccaff7653c7c4a606f3163eb921e5
nargiz <nargiz.nasirovar@gmail.com>
2024-06-28 23:22:33 +0200

    I added

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -3,22 +3,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def head():
-     return "Hello World Nargiz"
+    return "Hello Nargiz"
 
-@app.route("/secondpage")
-def second():
-     return "this is second page"
+@app.route("/second")
+def head2():
+    return "Hello Nargiz2"
 
-@app.route("/third")
-def third():
-     return "this is third page"
-
-@app.route("/fourth/<string:id>")
-def fourth(id):
-     return f"this is {id} page"
+@app.route("/forth/<string:id>")
+def forth(id):
+    return f"ID of this page is{id}"
 
 
 if __name__ == '__main__':
 
-     app.run(debug=True)
+     app.run(debug=True, port=30000)
      #app.run(host= '0.0.0.0', port=80)
--- flask-02-Jinja_Template/jinja.py
+++ flask-02-Jinja_Template/jinja.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+@app.route("/")
+def head():
+    return render_template("index.html", number1=123, number2=1234)
 
-
-
-
-
-
+@app.route("/aws-Prem")
+def number():
+     num1 = 12
+     num2 = 11
+     return render_template("body.html", value1 = num1, value2 = num2, sum = num1+num2)
 
 
 
 if __name__== "__main__":
      #app.run(debug=True, port=30000)
-     app.run(host= '0.0.0.0', port=80)
+     app.run(host= '0.0.0.0', port=8081)
